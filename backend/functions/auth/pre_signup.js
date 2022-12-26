f22984eeaa034d9b631ac305ee6292a8db51ffa5
Stefan Olaru <contact@stefanolaru.com>
2022-12-26 12:03:37 +0200

    only accept non-role company emails to signup

--- backend/functions/auth/pre_signup.js
+++ backend/functions/auth/pre_signup.js
@@ -10,6 +10,10 @@ exports.handler = async (event, context, callback) => {
         callback(new Error(result.error), event);
     } else if (result.is_disposable) {
         callback(new Error("Disposable emails are not accepted"), event);
+    } else if (result.is_free_email) {
+        callback(new Error("Please use your company email"), event);
+    } else if (result.is_role) {
+        callback(new Error("Please use a non role company email"), event);
     }
 
     // auto confirm the user and email on signup, they'll get credits on first login

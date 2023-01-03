308488874778b8708c5e99dac6d2bd2a7dbb1835
Stefan Olaru <contact@stefanolaru.com>
2023-01-03 21:34:49 +0200

    smtp validation refactor

--- backend/libs/validator.js
+++ backend/libs/validator.js
@@ -1,4 +1,5 @@
 const dns = require("dns"),
+    { SMTPClient } = require("smtp-client"),
     disposable = require("disposable-email-domains"),
     free = require("free-email-domains"),
     roles = [
@@ -56,29 +57,34 @@ const validateEmail = async (input) => {
             email: input,
             error: result.error,
         };
-    //
-    // check domain dns
-    const mxs = await mxQuery(result.domain)
-        .then()
-        .catch((err) => {
-            // console.log(err);
-            return [];
-        });
 
-    // return error if no MX
-    if (!mxs || !mxs.length) {
-        Object.assign(result, {
-            error: "No MX entries",
-        });
-        return result;
-    }
-
-    // append disposable & free checks
-    return Object.assign(result, {
+    // passed format validation, append disposable & free checks
+    Object.assign(result, {
         is_disposable: isDisposable(result.domain),
         is_free_email: isFreeMail(result.domain),
         is_role: isRoleMail(result.local_part),
     });
+
+    // check MX & SMTP
+    if (!result.is_disposable && !result.is_role) {
+        await validateDomain(result.domain, [result.local_part], 3000)
+            .then((res) => {
+                if (res.domain) {
+                    Object.assign(result, {
+                        error: res.domain,
+                    });
+                } else if (res.recipients) {
+                    Object.assign(result, {
+                        error: "Invalid email",
+                    });
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    return result;
 };
 /**
  *
@@ -196,8 +202,150 @@ const isFreeMail = (domain) => free.includes(domain);
  */
 const isRoleMail = (domain) => roles.includes(domain);
 
+/**
+ *
+ * @param {*} domain
+ * @param {*} recipients
+ * @param {*} timeout
+ * @returns Object containing domain issue OR invalid recipients / Empty object when no invalid entries were found
+ */
+const validateDomain = async (domain, recipients, timeout = 5000) => {
+    // vars
+    let client = null,
+        connected = false;
+
+    // check domain MX entries
+    let mxs = await mxQuery(domain, timeout)
+        .then()
+        .catch((err) => {
+            return [];
+        });
+
+    // return error if no MX
+    if (!mxs || !mxs.length) {
+        return Promise.resolve({
+            domain: "MX",
+        });
+    }
+
+    // try just the first 2 MX hosts
+    mxs = mxs.slice(0, 2);
+
+    // attempt to connect to one of mx hosts
+    while (!connected && mxs.length) {
+        const mx = mxs.shift();
+        // set connection settings
+        client = new SMTPClient({ host: mx.exchange, port: 25 });
+
+        // connect to mx
+        connected = await client
+            .connect({ timeout: timeout })
+            .then(() => true)
+            .catch((err) => {
+                return false;
+            });
+    }
+
+    // stop if still not connected
+    if (!connected) {
+        return Promise.resolve({
+            domain: "CONNECT",
+        });
+    }
+
+    // Send a HELO/EHLO command
+    const HELO = await client
+        .greet({ timeout: timeout })
+        .then(() => true)
+        .catch((err) => false);
+
+    // stop if HELO doesn't go through
+    if (!HELO) {
+        try {
+            await client.quit({ timeout: timeout });
+        } catch (err) {}
+        return Promise.resolve({
+            domain: "EHLO",
+        });
+    }
+
+    // Send a MAIL FROM command
+    const MAIL = await client
+        .mail({
+            from: process.env.EMAIL_FROM_ADDRESS,
+            timeout: timeout,
+        })
+        .then(() => true)
+        .catch((err) => false);
+
+    // stop if MAIL doesn't go through
+    if (!MAIL) {
+        try {
+            await client.quit({ timeout: timeout });
+        } catch (err) {}
+        return Promise.resolve({
+            domain: "MAIL",
+        });
+    }
+
+    // Send a RCPT TO command to a random recipient
+    const CATCH_ALL = await client
+        .rcpt({
+            to: "catch-all-" + Date.now() + "@" + domain,
+            timeout: timeout,
+        })
+        .then(() => true)
+        .catch(() => false);
+
+    // stop if it's a catch all domain, it will accept any recipient
+    if (CATCH_ALL) {
+        try {
+            await client.quit({ timeout: timeout });
+        } catch (err) {}
+        // resolve
+        return Promise.resolve({
+            domain: "CATCH_ALL",
+        });
+    }
+
+    // check for invalid recipients
+    const invalid = [];
+    while (recipients.length) {
+        const recipient = recipients.shift();
+        // Send a RCPT TO command to every recipient
+        await client
+            .rcpt({
+                to: recipient + "@" + domain,
+                timeout: timeout,
+            })
+            .then()
+            .catch((err) => {
+                invalid.push({
+                    local_part: recipient,
+                    error: err.code || null,
+                });
+            });
+    }
+
+    // rset & gracefully quit
+    try {
+        await client.rset({ timeout: timeout });
+        await client.quit({ timeout: timeout });
+    } catch (err) {}
+
+    // resolve empty object if no invalid recipients
+    return Promise.resolve(
+        invalid.length
+            ? {
+                  recipients: invalid,
+              }
+            : {}
+    );
+};
+
 module.exports = {
     validateEmail,
+    validateDomain,
     checkFormat,
     mxQuery,
     isDisposable,

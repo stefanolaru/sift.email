57ecec73af0355b78ccf0cf75199a002f7eb5bbe
Stefan Olaru <contact@stefanolaru.com>
2023-01-05 18:06:31 +0200

    list processor refactor

--- backend/functions/list_processor/bundle.js
+++ backend/functions/list_processor/bundle.js
@@ -1,168 +1,300 @@
 const AWS = require("aws-sdk"),
     s3 = new AWS.S3(),
-    Validator = require("../../libs/validator");
+    sns = new AWS.SNS(),
+    ddb = new AWS.DynamoDB(),
+    Papa = require("papaparse");
 
+// this gets triggered only via SNS, reads payload from message
 exports.handler = async (event) => {
-    console.log(event);
-    return;
-    let promises = [],
-        invalidDomains = [];
+    // get request_id
+    const msg = JSON.parse(event.Records[0].Sns.Message),
+        { request_id } = msg;
 
-    // read invalid domains
-    await s3
-        .listObjectsV2({
-            Bucket: process.env.S3_BUCKET,
-            Prefix:
-                "chunks/" +
-                event.meta.user_id +
-                "/" +
-                event.meta.request_id +
-                "/" +
-                "invalid-domains/",
+    // get the list validation request info
+    const request = await ddb
+        .query({
+            TableName: process.env.DDB_TABLE,
+            IndexName: "GSI",
+            KeyConditionExpression: "#GSI = :GSI AND #SK = :SK",
+            ExpressionAttributeNames: {
+                "#GSI": "GSI",
+                "#SK": "SK",
+            },
+            ExpressionAttributeValues: AWS.DynamoDB.Converter.marshall({
+                ":GSI": "request#private",
+                ":SK": "request#" + request_id,
+            }),
+            ScanIndexForward: false,
+            Limit: 1,
         })
         .promise()
         .then((res) => {
-            if (res.Contents) {
-                res.Contents.forEach((item) => {
-                    // add to promises
-                    promises.push(
-                        s3
-                            .getObject({
-                                Bucket: process.env.S3_BUCKET,
-                                Key: item.Key,
-                            })
-                            .promise()
-                    );
-                });
-            }
+            return res.Items.length
+                ? AWS.DynamoDB.Converter.unmarshall(res.Items[0])
+                : null;
         })
         .catch((err) => {
             console.log(err);
+            return null;
         });
 
-    if (promises.length) {
-        await Promise.allSettled(promises)
-            .then((res) => {
-                res.forEach((r) => {
-                    if (r.status == "fulfilled") {
-                        // populate chunks into invalid domains
-                        invalidDomains.push(
-                            ...JSON.parse(r.value.Body.toString("utf-8"))
-                        );
-                    }
-                });
-            })
-            .catch((err) => {
-                console.log(err);
-            });
+    // unlikely, but still
+    if (!request) {
+        return;
     }
 
-    // prep results object to finalize validation
-    const results = {},
-        chunkKeys = ["valid", "invalid", "pending", "duplicates"];
+    // extract user_id from PK
+    const user_id = request.PK.replace("user#", "");
 
-    // cleanup previous promises
-    promises = [];
+    // pull the processed items from the db table and add to db items
+    let lek = null,
+        dbItems = [];
 
-    // read chunks
-    chunkKeys.forEach((k) => {
-        promises.push(
-            s3
-                .getObject({
-                    Bucket: process.env.S3_BUCKET,
-                    Key:
-                        "chunks/" +
-                        event.meta.user_id +
-                        "/" +
-                        event.meta.request_id +
-                        "/" +
-                        k +
-                        ".json",
-                })
-                .promise()
-        );
-    });
+    // first do :)
+    do {
+        // default params
+        const params = {
+            TableName: process.env.DDB_TABLE,
+            KeyConditionExpression: "#PK = :PK",
+            ExpressionAttributeNames: {
+                "#PK": "PK",
+            },
+            ExpressionAttributeValues: AWS.DynamoDB.Converter.marshall({
+                ":PK": "dc#" + request_id,
+            }),
+            ScanIndexForward: false,
+            Limit: 250,
+        };
 
-    // wait for files to be read
-    await Promise.allSettled(promises)
-        .then((res) => {
-            res.forEach((r, idx) => {
-                if (r.status == "fulfilled") {
-                    // populate chunks into results
-                    results[chunkKeys[idx]] = JSON.parse(
-                        r.value.Body.toString("utf-8")
-                    );
-                } else {
-                    // set empty array, the file failed to read
-                    results[chunkKeys[idx]] = [];
-                }
+        // add ExclusiveStartKey if lek is present
+        if (lek) {
+            Object.assign(params, {
+                ExclusiveStartKey: lek,
             });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-
-    // validate pending, if any
-    if (results.pending.length) {
-        results.pending.forEach((item) => {
-            const res = Validator.checkFormat(item.data[3]);
-            if (invalidDomains.includes(res.domain)) {
-                results.invalid.push(
-                    Object.assign(item, {
-                        error: "No MX entries",
-                    })
-                );
-            } else {
-                results.valid.push(item);
-            }
-        });
-        // delete the key from results, not needed anymore
-        delete results.pending;
-    }
-
-    // cleanup previous promises
-    promises = [];
-
-    // prepare the output, upload results
-    const output = {
-        meta: event.meta,
-    };
-
-    // loop results keys and create files
-    Object.keys(results).forEach((k) => {
-        // update output with counter
-        output[k] = results[k].length;
-        //
-        if (output[k] > 0) {
-            promises.push(
-                s3
-                    .upload({
-                        Bucket: process.env.S3_BUCKET,
-                        Body: JSON.stringify(results[k]),
-                        Key:
-                            "output/" +
-                            event.meta.user_id +
-                            "/" +
-                            event.meta.request_id +
-                            "/" +
-                            k +
-                            ".json",
-                        ContentType: "application/json",
-                    })
-                    .promise()
-            );
         }
-    });
 
-    // wait for final s3 objects to be uploaded
-    if (promises) {
-        await Promise.allSettled(promises)
+        await ddb
+            .query(params)
+            .promise()
+            .then((res) => {
+                // update key
+                lek = res.LastEvaluatedKey ? res.LastEvaluatedKey : null;
+                //
+                if (res.Items.length) {
+                    // loop all items and push into dbItems
+                    res.Items.forEach((item) => {
+                        // unmarshal item
+                        item = AWS.DynamoDB.Converter.unmarshall(item);
+
+                        if (item.result) {
+                            // extract invalid local parts
+                            const invalidLocalParts = [];
+                            // process invalid recipients first
+                            if (item.result.recipients.invalid) {
+                                item.result.recipients.invalid.forEach((v) => {
+                                    invalidLocalParts.push(v.local_part);
+                                    // add to dbItems
+                                    dbItems.push({
+                                        result: "INVALID",
+                                        accuracy: v.accuracy,
+                                        note: "Recipient does not exist",
+                                        email: v.local_part + "@" + item.domain,
+                                    });
+                                });
+                            }
+                            // remove invalid localparts from localparts, extract difference
+                            const localParts = item.local_parts.filter(
+                                (x) => !invalidLocalParts.includes(x)
+                            );
+
+                            // if any valid local parts are left, add to dbItems
+                            if (localParts) {
+                                localParts.forEach((v) => {
+                                    const validRecipient = {
+                                        result: item.result.domain.result,
+                                        accuracy: item.result.domain.accuracy,
+                                        note: item.result.domain.note || "",
+                                        email: v + "@" + item.domain,
+                                    };
+                                    // add recipient to list
+                                    dbItems.push(validRecipient);
+                                });
+                            }
+                        }
+                    });
+                }
+            })
             .then()
             .catch((err) => {
                 console.log(err);
             });
-    }
+    } while (lek);
+
+    // read invalid results stored in S3 by prevalidator
+    const s3Items = await s3
+        .getObject({
+            Bucket: process.env.S3_BUCKET,
+            Key: "output/" + user_id + "/" + request_id + "/invalid.json",
+        })
+        .promise()
+        .then((res) => JSON.parse(res.Body.toString("utf-8")))
+        .catch((err) => {
+            console.log(err);
+            return [];
+        }); // return empty array if no items were dropped on prevalidation
+
+    // combine items from DB with the S3 ones
+    const resultItems = dbItems.concat(s3Items),
+        resultMap = {};
+    resultItems.forEach((v) => {
+        resultMap[v.email] = v;
+    });
+
+    // get the original parse data
+    const parsedJson = await s3
+        .getObject({
+            Bucket: process.env.S3_BUCKET,
+            Key: "parsed/" + user_id + "/" + request_id + "/data.json",
+        })
+        .promise()
+        .then((res) => JSON.parse(res.Body.toString("utf-8")))
+        .catch((err) => {
+            console.log(err);
+            return null;
+        });
+
+    // prepare the output
+    const valid = [],
+        invalid = [];
+
+    // loop the original JSON and append validation results
+    parsedJson.forEach((row, idx) => {
+        const key = row[request.column_idx].toLowerCase();
+        const res = resultMap[key];
+        if (res) {
+            // append result, accuracy, notes columns
+            row.push(...[res.result, res.accuracy, res.note]);
+            // set in categories
+            if (res.result == "VALID") {
+                valid.push(row);
+            } else {
+                invalid.push(row);
+            }
+        } else {
+            // append empty columns
+            row.push(...(!idx ? ["Result", "Accuracy", "Note"] : ["", "", ""]));
+        }
+    });
+
+    // write the validation stats to db before moving forward
+    const ts = new Date(),
+        stats = {
+            timestamp: ts.toISOString(),
+            invalid: invalid.length,
+            valid: valid.length,
+            total: parsedJson.length,
+        };
+    await ddb
+        .updateItem({
+            TableName: process.env.DDB_TABLE,
+            Key: AWS.DynamoDB.Converter.marshall({
+                PK: request.PK,
+                SK: request.SK,
+            }),
+            ExpressionAttributeNames: {
+                "#results": "validation_results",
+            },
+            ExpressionAttributeValues: AWS.DynamoDB.Converter.marshall({
+                ":value": stats,
+            }),
+            UpdateExpression: "SET #results=:value",
+        })
+        .promise()
+        .then()
+        .catch((err) => {
+            console.log(err);
+        });
+
+    // write the json results
+
+    const resultsS3Prefix = "output/" + user_id + "/" + request_id + "/results",
+        s3Promises = [
+            // full results JSON
+            s3
+                .upload({
+                    Bucket: process.env.S3_BUCKET,
+                    Body: JSON.stringify(parsedJson),
+                    Key: resultsS3Prefix + "/full.json",
+                    ContentType: "application/json",
+                })
+                .promise(),
+            // full results CSV
+            s3
+                .upload({
+                    Bucket: process.env.S3_BUCKET,
+                    Body: Papa.unparse(parsedJson),
+                    Key: resultsS3Prefix + "/full.csv",
+                    ContentType: "text/csv",
+                })
+                .promise(),
+            // valid results JSON
+            s3
+                .upload({
+                    Bucket: process.env.S3_BUCKET,
+                    Body: JSON.stringify(valid),
+                    Key: resultsS3Prefix + "/valid.json",
+                    ContentType: "application/json",
+                })
+                .promise(),
+            // valid results CSV
+            s3
+                .upload({
+                    Bucket: process.env.S3_BUCKET,
+                    Body: Papa.unparse(valid),
+                    Key: resultsS3Prefix + "/valid.csv",
+                    ContentType: "text/csv",
+                })
+                .promise(),
+            // invalid results JSON
+            s3
+                .upload({
+                    Bucket: process.env.S3_BUCKET,
+                    Body: JSON.stringify(invalid),
+                    Key: resultsS3Prefix + "/invalid.json",
+                    ContentType: "application/json",
+                })
+                .promise(),
+            // invalid results CSV
+            s3
+                .upload({
+                    Bucket: process.env.S3_BUCKET,
+                    Body: Papa.unparse(invalid),
+                    Key: resultsS3Prefix + "/invalid.csv",
+                    ContentType: "text/csv",
+                })
+                .promise(),
+        ];
+
+    await Promise.allSettled(s3Promises)
+        .then()
+        .catch((err) => {
+            console.log(err);
+        });
+
+    // trigger SNS topic
+    await sns
+        .publish({
+            Message: JSON.stringify(event),
+            TopicArn: process.env.SNS_TOPIC,
+        })
+        .promise()
+        .then()
+        .catch((err) => {
+            console.log(err);
+        });
+
+    // wooohoo, we're done!
 
     // return the stats
-    return output;
+    return stats;
 };
--- backend/functions/list_processor/prevalidate.js
+++ backend/functions/list_processor/prevalidate.js
@@ -1,6 +1,7 @@
 const AWS = require("aws-sdk"),
     ddb = new AWS.DynamoDB(),
     s3 = new AWS.S3(),
+    sns = new AWS.SNS(),
     eb = new AWS.EventBridge(),
     Validator = require("../../libs/validator"),
     Utils = require("../../libs/utils");
@@ -62,9 +63,9 @@ exports.handler = async (event) => {
     const column_idx = request.column_idx,
         uniqueRecipients = [],
         results = {
-            invalid: [], // invalid format
+            invalid: [], // invalid format, skipped, pushed to S3 invalid
             duplicates: [], // duplicates
-            pending: {}, // to be pushed to queue
+            pending: {}, // to be pushed to DynamoDB and for mx/smtp checks
         };
 
     // loop data and make the initial validation
@@ -72,11 +73,7 @@ exports.handler = async (event) => {
         const email = item[column_idx] ? item[column_idx].toLowerCase() : "";
         // if duplicate, drop
         if (uniqueRecipients.includes(email)) {
-            results.duplicates.push({
-                idx: idx,
-                data: item,
-                error: "Duplicate",
-            });
+            results.duplicates.push(item);
         } else {
             // add to unique recipients
             uniqueRecipients.push(email);
@@ -86,11 +83,15 @@ exports.handler = async (event) => {
 
             // add to invalid if error
             if (recipient.error) {
-                results.invalid.push({
-                    idx: idx,
-                    data: item,
-                    reason: "Invalid format",
-                });
+                // ignore first row format error, it's very likely the table header
+                if (idx) {
+                    results.invalid.push({
+                        result: "INVALID",
+                        accuracy: "HIGH",
+                        note: "Invalid format",
+                        email: email,
+                    });
+                }
             } else {
                 const domain = recipient.domain.toLowerCase();
                 // create domain key
@@ -100,15 +101,17 @@ exports.handler = async (event) => {
                 // drop directly if disposable or generic role
                 if (Validator.isDisposable(domain)) {
                     results.invalid.push({
-                        idx: idx,
-                        data: item,
-                        reason: "Disposable domain",
+                        result: "INVALID",
+                        accuracy: "HIGH",
+                        note: "Disposable domain",
+                        email: email,
                     });
                 } else if (Validator.isRoleMail(domain)) {
                     results.invalid.push({
-                        idx: idx,
-                        data: item,
-                        reason: "Role-based address",
+                        result: "INVALID",
+                        accuracy: "HIGH",
+                        note: "Role-based address",
+                        email: email,
                     });
                 } else {
                     // queue for further validation
@@ -120,7 +123,8 @@ exports.handler = async (event) => {
 
     // write to S3 initial results
     const promises = [],
-        output = {};
+        output = {},
+        ts = new Date();
 
     // loop results keys and create files
     ["invalid", "duplicates"].forEach((k) => {
@@ -134,7 +138,7 @@ exports.handler = async (event) => {
                         Bucket: process.env.S3_BUCKET,
                         Body: JSON.stringify(results[k]),
                         Key:
-                            "ouput/" +
+                            "output/" +
                             metadata.user_id +
                             "/" +
                             metadata.request_id +
@@ -151,6 +155,50 @@ exports.handler = async (event) => {
     // wait for s3 objects to be uploaded
     if (promises) {
         await Promise.allSettled(promises)
+            .then()
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    // write the prevalidation stats to db before moving forward
+    await ddb
+        .updateItem({
+            TableName: process.env.DDB_TABLE,
+            Key: AWS.DynamoDB.Converter.marshall({
+                PK: request.PK,
+                SK: request.SK,
+            }),
+            ExpressionAttributeNames: {
+                "#results": "prevalidation_results",
+            },
+            ExpressionAttributeValues: AWS.DynamoDB.Converter.marshall({
+                ":value": {
+                    timestamp: ts.toISOString(),
+                    duplicate: results.duplicates.length,
+                    invalid: results.invalid.length,
+                    pending: Object.keys(results.pending).length,
+                },
+            }),
+            UpdateExpression: "SET #results=:value",
+        })
+        .promise()
+        .then()
+        .catch((err) => {
+            console.log(err);
+        });
+
+    // if there are no recipients pending to mx/smtp validation skip directly to results bundling
+    // trigger ListValidationEndTopic SNS topic and STOP
+    if (!Object.keys(results.pending).length) {
+        return await sns
+            .publish({
+                Message: JSON.stringify({
+                    request_id: metadata.request_id,
+                }),
+                TopicArn: process.env.SNS_TOPIC,
+            })
+            .promise()
             .then()
             .catch((err) => {
                 console.log(err);
@@ -191,29 +239,27 @@ exports.handler = async (event) => {
     if (!ruleCreated) return;
 
     // prepare pending items for batch db insert
-    const ts = new Date(),
-        dbItems = [];
-    if (Object.keys(results.pending).length) {
-        Object.keys(results.pending).forEach((domain) => {
-            const shards = Utils.chunkArray(results.pending[domain], 50);
-            shards.forEach((v, k) => {
-                dbItems.push({
-                    PutRequest: {
-                        Item: AWS.DynamoDB.Converter.marshall({
-                            PK: "dc#" + metadata.request_id,
-                            SK: domain + "#" + k,
-                            GSI: metadata.request_id + "#pending",
-                            domain: domain,
-                            local_parts: v,
-                            created_at: Math.floor(ts / 1000),
-                            entity_type: "domain_check",
-                            expires_at: Math.floor(ts / 1000) + 24 * 3600, // TTL 24 hours from now
-                        }),
-                    },
-                });
+    const dbItems = [];
+
+    Object.keys(results.pending).forEach((domain) => {
+        const shards = Utils.chunkArray(results.pending[domain], 50);
+        shards.forEach((v, k) => {
+            dbItems.push({
+                PutRequest: {
+                    Item: AWS.DynamoDB.Converter.marshall({
+                        PK: "dc#" + metadata.request_id,
+                        SK: domain + "#" + k,
+                        GSI: metadata.request_id + "#pending",
+                        domain: domain,
+                        local_parts: v,
+                        created_at: Math.floor(ts / 1000),
+                        entity_type: "domain_check",
+                        expires_at: Math.floor(ts / 1000) + 24 * 3600, // TTL 24 hours from now
+                    }),
+                },
             });
         });
-    }
+    });
 
     // chunk array into chunks of 25
     const chunks = Utils.chunkArray(dbItems, 25);
